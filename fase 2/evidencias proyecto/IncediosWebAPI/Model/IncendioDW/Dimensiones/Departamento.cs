54b1b51883ea6c8492985dfc31023b2e89eda65c
Margari2061 <jo.jarap@duocuc.cl>
2025-10-29 20:09:36 -0300

    Departamento DW avance

--- fase 2/evidencias proyecto/IncediosWebAPI/Model/IncendioDW/Dimensiones/Departamento.cs
+++ fase 2/evidencias proyecto/IncediosWebAPI/Model/IncendioDW/Dimensiones/Departamento.cs
@@ -1,6 +1,33 @@
-﻿namespace IncediosWebAPI.Model.IncendioDW.Dimensiones
+﻿using IncediosWebAPI.Model.IncendioDB.Domain;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IncediosWebAPI.Model.IncendioDW.Dimensiones
 {
-    public class Departamento
+    public class DepartamentoDW
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Nombre { get; set; } = string.Empty;
+
+        
+        public int SedeId { get; set; } // FK a Sede (desnormalizado)
+
+        [MaxLength(100)]
+        public string SedeNombre { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        public string? SedeRegion { get; set; }
+
+        // Propiedades para análisis
+        public int TotalUsuarios { get; set; }
+        public int TotalPartidas { get; set; }
+        public double TasaExitoPromedio { get; set; }
+
+        // Navigation properties
+        public List<UsuarioDW> Usuarios { get; set; } = new();
     }
 }
